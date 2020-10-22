0f8341234a1ceaf142268def23562f3b30960250
Federico Zimmermann <48495525+fedezimm@users.noreply.github.com>
2020-10-22 20:49:23 -0300

    Agrego paso 5, instrucciones y refactorizo un touch.

--- knn_app/app.py
+++ knn_app/app.py
@@ -1,152 +1,194 @@
-from knn import *
 import streamlit as st
-import time
+import codecs
+from knn import *
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+def main():
+    # Renderizamos el encabezado de la aplicación con st.markdown
+    readme_text = st.markdown(get_file_content_as_string("instrucciones.md"))
 
+    # Agregamos un selector para el modo de la aplicación en la barra lateral
+    st.sidebar.title("TP Final - Grupo 5 - IA")
+    app_mode = st.sidebar.selectbox(
+        "Elige el modo de la aplicación",
+        ["Mostrar instrucciones", "App Mode"]
+    )
+    if app_mode == "Mostrar instrucciones":
+        st.sidebar.success("Para continuar selecciona 'App Mode' o 'Tests Mode'.")
+    elif app_mode == "App Mode":
+        readme_text.empty()
+        run_the_app()
+    #elif app_mode == "Tests Mode":
+        #readme_text.empty()
+        #run_tests()
 
-"""
-# Trabajo Final - Inteligencia Artificial 2020 - K-Nearest Neighbors
+def get_file_content_as_string(path):
+    """Función para leer un archivo"""
+    f = codecs.open(path,encoding='utf-8')
+    file = f.read()
+    f.close()
+    return file
 
-## Paso 1: Cargá tus datos y seleccioná la proporción de estos para el entrenamiento
+def run_the_app():
+    #Código de la aplicación
+    st.markdown('# Trabajo Final - Grupo 5 - Inteligencia Artificial 2020 - K-Nearest Neighbors')
+    step1()
 
-"""
-
-file = st.file_uploader('Cargá el archivo con los datos')
-if file is not None:
+def step1():
+    #--------------------------------------------------STEP 1--------------------------------------------------------------
+    st.markdown('## Paso 1: Carga y preparación de los datos.')
+    #Carga de archivo
+    file = st.file_uploader('Cargá el archivo con los datos')
     
-    sep = st.selectbox('Selecciona el separador de campos del archivo',[None, ",", ";", "Tab"])
-    if sep is not None:
-        
-        data = pd.read_csv(file, sep=sep)
+    if file is not None:
+        file.seek(0)
+        #Seleccionde separador
+        sep = st.selectbox('Selecciona el separador de campos del archivo',[None, ",", ";", "Tab"])
+        if sep is not None:  
+            #Carga de los datos en un pd.dataframe
+            data = pd.read_csv(file, sep=sep)
+            #Se extraen las etiquetas de las clases
+            labels = data.iloc[:,2].unique()
+            dictionary = {}
+            for i in range(len(labels)):
+                dictionary[labels[i]] = np.float(i+1)
+            #Se confirma la carga correcta de los datos
+            st.write('Los datos fueron cargados **CORRECTAMENTE** :heavy_check_mark:')
+            #Se muestran los datos al tildar el checkbox
+            if st.checkbox('Ver datos'):
+                st.dataframe(data)
+            #Se reemplazan los labels de los datos por numeros de 0 a N para poder manejar en np.ndarray
+            data = data.replace(to_replace = dictionary)
+            data = data.to_numpy()
+            st.write("Hay ", data.shape[0], ' puntos cargados')
+            #Selección de la proporcion de entrenamiento
+            train_prop = st.selectbox('Seleccione la proporción de datos de entrenamiento',
+                            [None,0.7,0.75,0.8,0.85]
+            )            
+            if train_prop is not None:                
+                st.write('La proporción de entrenamiento elegida es de: ', train_prop,'la de test es: ', round(1-train_prop,2))
+                #Selección del seed
+                seed = st.select_slider('Seleccione un valor de seed para mezclar los datos y dividir el conjunto.', list(range(1,51)))                
+                #División del conjunto en training y test
+                training, testing = split(data, train_prop, round(1-train_prop,2), seed=seed)
+                st.write('El seed elegido para mezclar los datos es de: ', seed)
+                #Opción de ver la distribución de los conjuntos
+                if st.checkbox('Ver Distribución de los conjuntos'):
+                    st.write(plot_training_test_distributions(training, testing))
+                    st.write("Hay ", len(training), " puntos de entrenamiento y ", len(testing), " puntos de testeo.")
+                    #Se ejecuta el paso 2.
+                    step2(data, training, testing)
 
-        labels = data.iloc[:,2].unique()
-        dictionary = {}
-        for i in range(len(labels)):
-            dictionary[labels[i]] = np.float(i+1)
+def step2(data, training, testing):
+    #------------------------------------------------- STEP 2 ----------------------------------------------------------
+    st.markdown('## Paso 2: Evaluar la precisión de los modelos según K crezca.')
+    # Seleccion del valor de N                
+    n = st.selectbox(
+        'Selecciona el valor de N',
+        [None] + list(range(1,len(data)))
+    )
+    if n is not None:
+        # Se testean los modelos con k de 1 a N. Se usan siempre los mismos datos de test.
+        coherences_multiple_k = test_multiple_knn(training, testing, lastk=n)
+        # Se grafica K vs Accuracy obtenido
+        multiple_k_graph = plot_multiple_k(coherences_multiple_k)
+        st.write(multiple_k_graph)
+        #Se ejecuta el paso 3.
+        step3(coherences_multiple_k, training, testing, n)
 
-        st.write('Los datos fueron cargados **CORRECTAMENTE** :heavy_check_mark:')
-        
+def step3(coherences_multiple_k, training, testing, n):
+    #--------------------------------------------------------------- STEP 3 ---------------------------------------------------------------
+    st.markdown('## Paso 3: Evaluar los mejores modelos KNN obtenidos y sus grillas.')
+    # Se determina el/los mejor/es K con el mejor valor de coherencia.
+    bests_k, best_coherence= determine_best_k(coherences_multiple_k)
+    st.write('El máximo valor de coherencia encontrado es: ', best_coherence)
+    #Se arma una cadena para mostrarla. Esta cadena dice los mejores K encontrados en el caso de que haya mas que uno.
+    cad = ''
+    for i in range(len(bests_k)):
+        if i == len(bests_k) - 2:
+            cad += str(bests_k[i]) + ' y '
+        elif i == len(bests_k) - 1:
+            cad += str(bests_k[i]) + '.'
+        else:
+            cad += str(bests_k[i]) + ', '                               
+    #Se extrae el primer valor de la lista para mostrar el mejor K en el caso de que sea uno solo
+    best_k = bests_k[0]
+    if len(bests_k) != 1:
+        st.write('Los modelos que llegan al máximo valor de coherencia tienen K = ', cad)  
+    else:
+        st.write('El modelo con mayor valor de coherencia tiene K = ', best_k)
+    #Seleccion de uno de los mejores valores de K para graficar el grid.                        
+    best_k = st.selectbox('Seleccione el mejor K para obtener su Grid de predicción', bests_k)
+    #Seleccion del formato del gráfico
+    rta = st.radio(
+        'Seleccione el formato del gráfico',
+        ("Normal","Con valores de confianza"), key=1
+    )
+    #Seleccion de la resolución del gráfico. 
+    h1 = st.selectbox(
+        'Seleccione la resolución del gráfico',
+        [None,0.1,0.25,0.5], key=1
+    )
+    plot_format = 'normal' if rta == 'Normal' else 'confidence'
+    if h1 is not None:
+        #Generación de la grilla y gráfico.
+        xx, yy, prediction_grid, confidence_grid, grid_graph = knn_prediction_grid(training, k=best_k, h=h1, plot = True, plot_format=plot_format, testing=testing)
+        st.pyplot(grid_graph)
+        #Se ejecuta el paso 4.
+        step4(n, training, testing, best_k, coherences_multiple_k)
 
-        if st.checkbox('Ver datos'):
-            #fig = plt.figure(figsize=(10,10))
-            #observation_colormap = ListedColormap (["red","green","blue","darkorange","purple"])
-            #plt.scatter(data[:,0:1], data[:,1:2])
-            st.dataframe(data)
-            #st.pyplot(fig)
-        data = data.replace(to_replace = dictionary)
-        data = data.to_numpy()
-
-        
-        
-        st.write("Hay ", data.shape[0], ' puntos cargados')
-
-
-        train_prop = st.selectbox('Seleccione la proporción de datos de entrenamiento',
-                        [None,0.7,0.75,0.8,0.85]
+def step4(n, training, testing, best_k, coherences_multiple_k):
+    #----------------------------------------------------------------------- STEP 4 ----------------------------------------------------------
+    st.markdown('## Paso 4: Evaluar una grilla con un valor distinto de K.')
+    # Selección de K
+    k_selected = st.selectbox(
+        'Selecciona el valor de K',
+        [None] + list(range(1,n+1))
+    )
+    if k_selected is not None:
+        # Selección del formato de gráfico
+        rta2 = st.radio(
+            'Seleccione el formato del gráfico',
+            ("Normal","Con valores de confianza"), key=2
         )
-        
-        if train_prop is not None:
-            
-            st.write('La proporción de entrenamiento elegida es de: ', train_prop,'la de test es: ', round(1-train_prop,2))
-            seed = st.select_slider('Seleccione un valor de seed para mezclar los datos y dividir el conjunto.', list(range(1,51)))
-            
-            training, testing = split(data, train_prop, round(1-train_prop,2), seed=seed)
-            
-            st.write('El seed elegido para mezclar los datos es de: ', seed)
-
-            if st.checkbox('Ver Distribución de los conjuntos'):
-                st.write(plot_training_test_distributions(training, testing))
-                st.write("Hay ", len(training), " puntos de entrenamiento y ", len(testing), " puntos de testeo.")
-            """
-            ## Paso 2: Con los datos cargados se entrenarán modelos K-nearest Neighbors con K de 1 a N.
-
-            """
-            
-            n = st.selectbox(
-                'Selecciona el valor de N',
-                [None] + list(range(1,len(data)))
-            )
-            if n is not None:
-
-                coherences_multiple_k = test_multiple_knn(training, testing, lastk=n)
-
-                multiple_k_graph = plot_multiple_k(coherences_multiple_k)
-
-                st.write(multiple_k_graph)
-                """
-                ## Paso 3: Se mostrará el mejor valor de K encontrado y el Grid de predicciones generado con este valor de K.
-                
-                """
-                bests_k, best_coherence= determine_best_k(coherences_multiple_k)
-                'El máximo valor de coherencia encontrado es: ', best_coherence
-                cad = ''
-                for i in range(len(bests_k)):
-                    if i == len(bests_k) - 2:
-                        cad += str(bests_k[i]) + ' y '
-                    elif i == len(bests_k) - 1:
-                        cad += str(bests_k[i]) + '.'
-                    else:
-                        cad += str(bests_k[i]) + ', '
-                
-                best_k = bests_k[0]
-
-                if len(bests_k) != 1:
-                    st.write('Los modelos que llegan al máximo valor de coherencia tienen K = ', cad)  
-                else:
-                    st.write('El modelo con mayor valor de coherencia tiene K = ', best_k)
-                
-                best_k = st.selectbox('Seleccione el mejor K para obtener su Grid de predicción', bests_k)
-
-                rta = st.radio(
-                    'Seleccione el formato del gráfico',
-                    ("Normal","Con valores de confianza"), key=1
-                ); h1 = st.selectbox(
-                    'Seleccione la resolución del gráfico',
-                    [None,0.1,0.25,0.5], key=1
-                )
-                plot_format = 'normal' if rta == 'Normal' else 'confidence'
-
-                if h1 is not None:
-                    xx, yy, prediction_grid, confidence_grid, grid_graph = knn_prediction_grid(training, k=best_k, h=h1, plot = True, plot_format=plot_format, testing=testing)
-
-                    st.pyplot(grid_graph)
-
-                    """
-                    ## Paso 4: Seleccioná cualquier K para comparar los Grids.
-                    
-                    """
-                    
-                    k_selected = st.selectbox(
-                        'Selecciona el valor de K',
-                        [None] + list(range(1,n+1))
-                    )
-
-                    if k_selected is not None:
-
-                        rta2 = st.radio(
-                            'Seleccione el formato del gráfico',
-                            ("Normal","Con valores de confianza"), key=2
-                        ); h2 = st.selectbox(
-                            'Seleccione la resolución del gráfico',
-                            [None, 0.1,0.25,0.5], key=2
-                        )
-
-                        if h2 is not None:
-
-                            plot_format2 = 'normal' if rta2 == 'Normal' else 'confidence'
-
-                            'Tu valor seleccionado de K es:', k_selected
-                            'El valor de coherencia para K=',k_selected,' es de ', coherences_multiple_k[k_selected-1]
-
-                            xx2, yy2, prediction_grid2, confidence_grid2, grid_graph2 = knn_prediction_grid(training, k=k_selected, h=h2, plot = True, plot_format=plot_format2, testing=testing)
-
-                            st.pyplot(grid_graph2)
-
-                            """
-                            ## Paso 5 - Cargá las coordenadas de nuevos puntos que quieras predecir.
-                            * Acá se mostrará la predicción del/ de los punto/s cargado/s y la confianza de esa/s predicción/es con:
-                                * El modelo del mejor K (Paso 3) y, 
-                                * El modelo del K seleccionado (Paso 4).
-                            """
-                    
+        # Selección de la resolución del gráfico
+        h2 = st.selectbox(
+            'Seleccione la resolución del gráfico',
+            [None, 0.1,0.25,0.5], key=2
+        )
+        if h2 is not None:
+            plot_format2 = 'normal' if rta2 == 'Normal' else 'confidence'
+            st.write('Tu valor seleccionado de K es:', k_selected)
+            st.write('El valor de coherencia para K=',k_selected,' es de ', coherences_multiple_k[k_selected-1])
+            # Generación del grid y graficación
+            xx2, yy2, prediction_grid2, confidence_grid2, grid_graph2 = knn_prediction_grid(training, k=k_selected, h=h2, plot = True, plot_format=plot_format2, testing=testing)
+            st.pyplot(grid_graph2)
+            # Se ejecuta el paso 5.
+            step5(training, best_k, k_selected)
+def step5(training, best_k, k_selected):
+    #------------------------------------------------------------- STEP 5 -------------------------------------------------------------- 
+    st.markdown('## Paso 5: Carga de un nuevo punto a predecir.')
+    # Selección de la coordenada X del punto.
+    x_new = st.number_input(
+        'Selecciona la coordenada X del nuevo punto a predecir',
+        min_value= float(np.min(training[:,0]) - 3.0),
+        max_value= float(np.max(training[:,0]) + 3.0),
+        value=0.0
+    )
+    if x_new != 0.0:
+        # Seleccion de la coordenada Y del punto
+        y_new = st.number_input(
+            'Selecciona la coordenada Y del nuevo punto a predecir',
+            min_value= float(np.min(training[:,1]) - 3.0),
+            max_value= float(np.max(training[:,1]) + 3.0),
+            value=0.0
+        )
+        if y_new != 0.0:
+            # Generación del nuevo punto
+            new_point = np.array([x_new, y_new]).transpose()
+            # Predicción con el mejor K seleccionado (paso 3).
+            best_predict = knn_predict(training, new_point, best_k)
+            # Predicción con el K seleccionado en paso 4.
+            predict2 = knn_predict(training, new_point, k_selected)
+            st.write('Con K = ',best_k,' el nuevo punto pertenece a la clase ',best_predict[0],', con una confianza de: ',round(best_predict[1],2))
+            st.write('Con K = ',k_selected,' el nuevo punto pertenece a la clase ',predict2[0],', con una confianza de: ',round(predict2[1],2))
+main()
---
+++ knn_app/instrucciones.md
@@ -0,0 +1,33 @@
+# Trabajo Final - Grupo 5 - Inteligencia Artificial 2020 - K-Nearest Neighbors
+
+## Modo App
+
+### Paso 1: Carga y preparación de los datos.
+* **1.1**: Cargar archivo **".csv" o ".txt"** con los datos que se usarán para analizar los modelos KNN que se construirán. El formato del archivo debe ser: valorX,valorY,valorClase. Teniendo en este ejemplo, la coma como separador. También se debe añadir nombre a las columnas.
+* **1.2**: Indicar el **separador** de campos del archivo (";", ",", "Tab").
+* **1.3**: Se indica que los datos fueron cargados con **éxito**. Se puede comprobar esto haciendo click en la casilla **"Ver Datos"**. También se indica cuántos puntos (filas) tenía el archivo cargado.
+* **1.4**: Seleccionar la **proporción** de los datos a **entrenar** (0.7, 0.75, 0.8 y 0.85 son las opciones disponibles,los puntos restantes se usarán para validar los modelos).
+* **1.5**: Elegir el **seed** o **semilla** que se usará para mezclar los datos y así obtener los dos conjuntos (training y test). Esto se pide para tener en todo momento de ejecución los mismos puntos de training y test. La forma de obtenerlos es mezclando los datos aleatoriamente y luego cortando y dividiendo justo en la proporción antes indicada. El seed o semilla sólo puede variar entre 1 y 50 para mayor simplicidad.
+* **1.6**: Se confirmará el valor de seed seleccionado y se podrá tildar la casilla **"Ver distribución de los conjuntos"** para poder analizar las distribuciones de los conjuntos de test y training, para así comprobar que se comportan de forma similar. Si esto no es así, se podrá variar el valor de seed, hasta que se encuentre proporciones deseadas.
+
+### Paso 2: Evaluar la precisión de los modelos según K crezca.
+* **2.1**: Seleccionar valor de **N**. Este valor será el **límite de K** hasta el cual se calcularán modelos. Es decir se calcularán modelos KNN con K de 1 a N (parámetro a seleccionar). Este valor de N puede ser hasta la cantidad total de datos menos 1.
+* **2.2**: Se mostrará el **gráfico** de las precisiones o valores de coherencia de los modelos con K variando de 1 a N. Este es un **gráfico de línea** con **K** en el **eje X** y la **precisión** del modelo en el **eje Y**.
+
+### Paso 3: Evaluar los mejores modelos KNN obtenidos y sus grillas.
+* **3.1**: Se informará la **máxima precisión alcanzada** y el **valor** o los **valores de K** para el/los cual/es se alcanza esa precisión.
+* **3.2**: Seleccionar uno de los mejores valores de K encontrados (en el caso de que haya más de uno).
+* **3.3**: Seleccionar qué opción de gráfico se quiere ver. **Normal** (grilla normal, la cual es preferible porque lleva menos tiempo) o **Con valores de confianza** (aquí, el gráfico variará de intensidad en los colores según el valor de confianza de las predicciones hechas por el modelo, cuanto más confianza haya en la predicción, más oscuro será el color).
+* **3.4**: Seleccionar la **resolución** del gráfico. En este caso se refiere al valor de **"Step"** o **"Paso"** de la grilla. Cuanto más chico, mayor resolución habrá (se generarán celdas más pequeñas). Las opciones disponibles son 0.1, 0.25 o 0.5. Para un gráfico con valores de confianza se **recomienda** elegir 0.25 o 0.5. De otra manera, se tardará mucho en mostrar el gráfico.
+* **3.5**: Aquí se muestra la **grilla** para los parámetros pasados. Los colores del fondo indican la predicción del modelo segun los datos de entrenamiento. Los puntos redondos son los datos de entrenamiento del modelo. Los puntos con forma de x son los puntos de validación del modelo.
+
+### Paso 4: Evaluar una grilla con un valor distinto de K.
+* **4.1**: Seleccionar un valor de K requerido. Con este valor se generará otra **grilla** a mostrar. Este paso, está pensado para **comparar** la grilla anterior (con la mejor precisión) con cualquier otra grilla con un valor de K distinto. Se podría aquí pedir un valor de K que **no tenga buen valor de precisión**, para poder comparar los modelos. Para poder ver qué valor de K no tiene una buena precisión, se podría ir a ver al gráfico del **paso 2**.
+* **4.2**: Seleccionar el formato del gráfico.
+* **4.3**: Seleccionar la resolución del gráfico. 
+* **4.4**: Se informará el valor de coherencia del modelo y se mostrará la grilla solicitada.
+
+### Paso 5: Carga de un nuevo punto a predecir.
+* **5.1**: Introducir un valor de coordenada X del nuevo punto.
+* **5.2**: Introducir un valro de coordenada Y del nuevo punto.
+* **5.4**: Se informa al usuario las predicciones del punto solicitado en ambos modelos. Estos son, los modelos con K solicitados en el paso 3 y 4. Se muestra la clase predicha por los modelos y el valor de confianza en las predicciones.
