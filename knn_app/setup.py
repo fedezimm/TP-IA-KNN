9eab273286dd363c62c22a46c068220a4e825cd4
Federico Zimmermann <48495525+fedezimm@users.noreply.github.com>
2020-10-30 12:04:44 -0300

    Agrego dependencia scikit-learn

--- knn_app/setup.py
+++ knn_app/setup.py
@@ -6,7 +6,7 @@ setup(
    version='1.0',
    description='A KNN grapher',
    author='Team N5 - IA - UTN',
-   author_email='fedezimm@gmail.com',
-   install_requires=['numpy','matplotlib','streamlit','pandas']
+   author_email=['fedezimm@gmail.com','carlosszaracho@gmail.com','geraenrique97@hotmail.com','nikoalegre97@gmail.com'],
+   install_requires=['numpy','matplotlib','streamlit','pandas','scikit-learn']
  
 )
